bf3e11a7d2ce357e9967bfaa482a1aaacfbdb1d2
abwlodar <abwlodar@gmail.com>
2024-07-22 10:52:03 +0200

    Add UTF-8 support

--- DownloadTicketsGenerateHTML.ps1
+++ DownloadTicketsGenerateHTML.ps1
@@ -83,7 +83,7 @@ function Process-Tickets {
             Write-Host "Failed to retrieve comments for Ticket ID: $($ticket.id) after $maxRetries retries."
             continue
         }
-        $htmlContent = "<html><body>"
+        $htmlContent = "<html><head><meta charset='UTF-8'><body>"
         $htmlContent += "<h1>Ticket ID: $($ticket.id)</h1>"
         $htmlContent += "<p><strong>Subject:</strong> $($ticket.subject)</p>"
         $htmlContent += "<p><strong>Description:</strong> $($ticket.description)</p>"
